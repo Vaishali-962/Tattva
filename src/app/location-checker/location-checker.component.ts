b1c4edfcfaa9a37ff4064db5e97f0cae7ef746d2
Vaishali-962 <vaishaliwankhade69200@gmail.com>
2026-02-11 15:56:05 +0530

    environment is set up and new location-checker component is created

---
+++ src/app/location-checker/location-checker.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-location-checker',
+  standalone: true,
+  imports: [],
+  templateUrl: './location-checker.component.html',
+  styleUrl: './location-checker.component.css'
+})
+export class LocationCheckerComponent {
+
+}
