7a6c9b847b615fc14c8199b58cc1b580b92e9f43
Vaishali-962 <vaishaliwankhade69200@gmail.com>
2026-02-11 15:59:30 +0530

    component bridge between location-checker and app component is created successfully

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LocationCheckerComponent } from './location-checker/location-checker.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LocationCheckerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
